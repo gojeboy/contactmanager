d0f041673edac4d70165c2814fe7fa0a0d7b3589
Njabulo Nsibande <njabsoul@gmail.com>
2019-05-22 03:06:49 +0200

    Updates

---
+++ precache-manifest.c7ace75d0da1edf10dc3fa199d777d8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "976b671928a506e65181a5dd31b3b62c",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "fb53b133044ebcd05ef1",
+    "url": "/contactmanager/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "b1e912385d6fd4c10eb2",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "fb53b133044ebcd05ef1",
+    "url": "/contactmanager/static/js/2.f061bcab.chunk.js"
+  },
+  {
+    "revision": "b1e912385d6fd4c10eb2",
+    "url": "/contactmanager/static/js/main.c033fe7d.chunk.js"
+  },
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  }
+]);
